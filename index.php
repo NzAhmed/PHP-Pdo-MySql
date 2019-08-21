e213f9ab72b79afa1768bed67cd06e3f5eb7d227
nzahmed <internetissue@gmail.com>
2019-08-21 14:24:23 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+    1- C:\xampp\mysql\bin>mysql -uroot
+    2- MariaDB [(none)]> create database shopping;
+    3- MariaDB [shopping]> create table products(id int primary key auto_increment,name varchar(50));
+    */
+
+    $pdo = new PDO("mysql:host=localhost;dbname=shopping","root","");
+
+    $stmt = $pdo->prepare("INSERT INTO Products(Name) VALUES ('Toyota')");
+    $stmt->execute();
+
+    $stmt = $pdo->prepare("SELECT * FROM Products");
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_OBJ);
+    foreach ($data as $key => $value) {
+        echo $value->name . "\n";
+    }
+?>
